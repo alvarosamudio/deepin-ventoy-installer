6514461a7bb46b6c93343dccb6fa5e839315f4fb
Alvaro Samudio <alvarosamudio@protonmail.com>
2026-07-22 18:12:38 -0500

    feat: update main.cpp for Qt6 compatibility
    - Remove Qt::AA_UseHighDpiPixmaps (always enabled in Qt6)
    - Remove deprecated loadDXcbPlugin comment
    - Bump version to 1.1.0
    - Clean up Chinese comments

--- main.cpp
+++ main.cpp
@@ -1,32 +1,30 @@
 #include "mainwindow.h"
 #include "utilities.h"
+
 #include <DApplication>
-#include <DGuiApplicationHelper>
-#include <DWidgetUtil> //Dtk::Widget::moveToCenter(&w); 要调用它，就得引用DWidgetUtil
+#include <DWidgetUtil>
 
 DWIDGET_USE_NAMESPACE
+
 int main(int argc, char *argv[]) {
-  // DApplication::loadDXcbPlugin();  //让bar处在标题栏中
   DApplication a(argc, argv);
 
-  a.setAttribute(Qt::AA_UseHighDpiPixmaps);
-  a.loadTranslator(QList<QLocale>() << QLocale::Spanish << QLocale::Chinese << QLocale::Portuguese << QLocale::Italian << QLocale::French);
+  a.loadTranslator(QList<QLocale>() << QLocale::Spanish << QLocale::Chinese
+                                    << QLocale::Portuguese << QLocale::Italian
+                                    << QLocale::French);
   a.setOrganizationName("0x4C57 Technology Group");
-  a.setApplicationVersion(DApplication::buildVersion("1.0.1"));
-  // a.setApplicationAcknowledgementPage("https://你的网站");
-  a.setProductIcon(QIcon(":/images/res/dvtoyinst.png")); // 设置Logo
+  a.setApplicationVersion(DApplication::buildVersion("1.1.0"));
+  a.setProductIcon(QIcon(":/images/res/dvtoyinst.png"));
   a.setProductName(QObject::tr("Deepin Ventoy Installer"));
   a.setApplicationDescription(
       QObject::tr("A Ventoy installer front-end written with DTK."));
-  a.setApplicationName(
-      "Deepin Ventoy Installer"); // 只有在这儿修改窗口标题才有效
+  a.setApplicationName("Deepin Ventoy Installer");
 
   rerunOnNonRoot();
 
   MainWindow w;
   w.show();
 
-  // 让打开时界面显示在正中
   Dtk::Widget::moveToCenter(&w);
 
   return a.exec();
